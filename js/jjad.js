ab2d1f58657b34b30f2002e9dc7d58a08be66dd5
Administrator <530454176@qq.com>
2017-06-05 08:56:35 +0800

    我的个人简历

---
+++ js/jjad.js
@@ -0,0 +1,16 @@
+document.writeln("<link type=\"text/css\" rel=\"stylesheet\" href=\"http://www.cssmoban.com/statics/css/astyle.css\" />");
+document.writeln("<div align=\"center\">");
+document.writeln("<script async src=\"../css/f2.txt\"></script>");
+document.writeln("<!-- cssmoban_728*90_show -->");
+document.writeln("<ins class=\"adsbygoogle\"");
+document.writeln("     style=\"display:inline-block;width:728px;height:90px\"");
+document.writeln("     data-ad-client=\"ca-pub-1542822386688301\"");
+document.writeln("     data-ad-slot=\"3852412413\"></ins>");
+document.writeln("<script>");
+document.writeln("(adsbygoogle = window.adsbygoogle || []).push({});");
+document.writeln("</script>");
+document.writeln("</div>");
+
+/**
+ * Created by Administrator on 2017/5/26.
+ */
